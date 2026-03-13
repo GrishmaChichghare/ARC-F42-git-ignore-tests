7575f469d9010786677844d1b19109b3b216af9e
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-03-13 15:20:31 +0530

    update  to index.html

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+// eslint-disable-next-line react-refresh/only-export-components
+const App = ()=>{
+
+  // eslint-disable-next-line no-unused-vars
+  let name = "Rajesh";
+
+  return(
+    // <h1> Hello App : {name}</h1>
+    <div>
+      <h1>Hello App</h1>
+      <h3>Hello Class</h3>
+    </div>
+  )
+}
+
+
+export default App;
