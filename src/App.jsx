8de459be9252743459c953b1cf39a941febec09a
Grishma Ashok Chichghare <chichgharegrishma1109@gmail.com>
2026-03-21 14:50:54 +0530

    update to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 
 import "./App.css";
 import Categories from "./components/categories";
-import Nav from "./components/nav";
-import SubCategories from "./components/subCategories";
 
 const usersDetails = [
     {
@@ -15,7 +13,7 @@ const usersDetails = [
 {
     id : 2,
     name : "vishnu",
-    status : "active",
+    status : "deactive",
     email : "vishnu@gmail.com",
     domain : "Fullstack"
 },
@@ -36,29 +34,11 @@ const usersDetails = [
 ]
 const MyApp = () => {
 
-    const displayList = (each) => {
-        const { id, name, email, domain } = each;
-
-        return(
-            <li key = {id} className="my-list p-3 shadow w-50 rounded-lg">
-                
-                <div className="profile">
-                    <h3>{name[0]}</h3>
-                </div>
-                <div className="my-content border-left border-danger ml-3 p-3">
-                    <h3>{name}</h3>
-                    <b>Email: {email}</b>
-                    <br />
-                    <span>Domain: {domain}</span>
-                </div>
-            </li>
-        ) 
-        
-    }
+    
     return(
       <ul className="main-cont">
         {
-            usersDetails.map( each => displayList(each))
+            usersDetails.map( each => <Categories key = {each.id} user = {each}/>)
         }
     </ul>
     )
--- src/components/categories/index.jsx
+++ src/components/categories/index.jsx
@@ -1,3 +1,19 @@
-const Categories = () => <h1>Categories Component</h1>;
+const Categories = ({user}) => {
+    const {name,email,domain} = user;
+    return(
+        <li className="my-list p-3 shadow w-50 rounded-lg text-red-50">
+            <div className="profile">
+                <h1>{name[0]}</h1>
+            </div>
 
+            <div className="my-content border-left border-danger ml-3 p-3">
+                <h3>{name}</h3>
+                <b>Email : {email}</b>
+                <br />
+                <span> Domain : {domain}</span>
+            </div>
+        </li>
+
+    )
+}
 export default Categories;
